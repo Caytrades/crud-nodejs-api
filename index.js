c247cb3e217d3480b56afed42a933ee7f04ad6fb
Caytrades <nanakwameopokuababio@gmail.com>
2024-11-02 07:10:55 +0000

    Added

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ app.get('/', (req, res) => {
   res.send('Hello World')
 })
 
+app.post('/', (req, res) => {
+    res.send('Added Hello World')
+})
+  
+
 app.listen(8000, () => {
     console.log("Server is running on http://localhost:8000")
 })
