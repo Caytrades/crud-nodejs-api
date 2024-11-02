43f30494028a7ebfc56625d6aa48e34b2b765aad
Caytrades <nanakwameopokuababio@gmail.com>
2024-11-02 21:41:30 +0000

    DB added

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-const express = require("express")
+const express = require("express");
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+
 const app = express()
+// Define the MongoDB connection URI using the environment variable
+const mongoURI = process.env.MONGODB_URI;
 
 app.get('/', (req, res) => {
   res.send('Hello World')
-})
+});
 
-app.post('/', (req, res) => {
-    res.send('Added Hello World')
+mongoose.connect(mongoURI)
+.then(() => {
+    console.log("Connected to the Database")
+    app.listen(8000, () => {
+        console.log("Server is running on http://localhost:8000")
+    });
 })
-  
-
-app.listen(8000, () => {
-    console.log("Server is running on http://localhost:8000")
-})
+.catch(() => {
+    console.log("Connection failed")
+});
---
+++ models/product.model.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const productSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, "Please enter product name"],
+        },
+
+        quantity: {
+            
+        }
+    }
+);
